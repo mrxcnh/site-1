0ba0e0ad13cd902a40e5627f57f559f9b80da555
mrxcnh <thanhdovan210184@gmail.com>
2021-10-30 10:47:58 +0700

    Update signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL <i className="fab fa-typo3"></i>
+            MonsEner <i className="fab fa-typo3"></i>
           </Link>
 
           <div className="menu-icon" onClick={handleClick}>
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -5,53 +5,44 @@ import './Signup.css';
 
 export default function SignUp() {
 
-    return (
-        <div className="signup-container">
-            <div className="registration-form">
-                <header>
-                    <h1>Sign Up</h1>
-                    <p>Fill in all informations</p>
-                </header>
-                <form>
-                    <div className="input-section email-section">
-                        <input
-                            className="email"
-                            type="email"
-                            placeholder="ENTER YOUR E-MAIL HERE"
-                            autocomplete="off"
-                        />
-                        <div className="animated-button">
-                            <span className={"icon-paper-plane "}><i className="fa fa-envelope"></i></span>
-                            <span className="next-button email"><i className="fa fa-arrow-up"></i></span>
-                        </div>
-                    </div>
-                    <div className={"input-section password-section "}>
-                        <input
-                            className="password"
-                            type="password"
-                            placeholder="ENTER YOUR PASSWORD HERE"
-                        />
-                        <div className="animated-button">
-                            <span className={"icon-lock "}><i className="fa fa-lock"></i></span>
-                            <span className="next-button password"><i className="fa fa-arrow-up"></i></span>
-                        </div>
-                    </div>
-                    <div className={"input-section repeat-password-section "}>
-                        <input
-                            className="repeat-password"
-                            type="password"
-                            placeholder="REPEAT YOUR PASSWORD HERE"
-                        />
-                        <div className="animated-button">
-                            <span className={"icon-repeat-lock "}><i className="fa fa-lock"></i></span>
-                            <span className="next-button repeat-password"><i className="fa fa-paper-plane"></i></span>
-                        </div>
-                    </div>
-                    <div className="success">
-                        <p>ACCOUNT CREATED</p>
-                    </div>
-                </form>
-            </div>
-            <Footer />
-        </div>)
+  return (
+    <div className="signup-container">
+      <div className="registration-form">
+        <header>
+          <h1>Sign Up</h1>
+          <p>Fill in all informations</p>
+        </header>
+        <form className="form-container">
+          <div className="input-section email-section">
+            <i className="fa fa-envelope"></i>
+            <input
+              className="email"
+              type="email"
+              placeholder="ENTER YOUR E-MAIL HERE"
+              autocomplete="off"
+            />
+          </div>
+          <div className={"input-section password-section "}>
+            <i className="fa fa-lock"></i>
+            <input
+              className="password"
+              type="password"
+              placeholder="ENTER YOUR PASSWORD HERE"
+            />
+          </div>
+          <div className={"input-section repeat-password-section "}>
+            <i className="fa fa-lock"></i>
+            <input
+              className="repeat-password"
+              type="password"
+              placeholder="REPEAT YOUR PASSWORD HERE"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign up
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </div>)
 }
